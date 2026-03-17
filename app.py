66f021c0dcae8032a65bb7185890d618323fc525
ansh3108 <anshkumar9686@gmail.com>
2026-03-17 12:14:34 +0530

    fixes for /iss

--- app.py
+++ app.py
@@ -7,22 +7,36 @@ NASA_API_KEY = 'HJ9wMZq8bcduhcQCP8jKywhrKiUrK2sXRMwJLk3P'
 
 @app.route('/')
 def index():
-    url  = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
+    url = f"https://api.nasa.gov/planetary/apod?api_key={NASA_API_KEY}"
     res = requests.get(url)
     data = res.json()
     return render_template('index.html', data=data)
 
 @app.route('/mars')
 def mars():
-    url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/latest_photos?api_key={NASA_API_KEY}"
-    response= requests.get(url)
-    data = response.json()
-    photos= data.get('latest_photos', [])[:12]
-    return render_template('mars.html', photos=photos)
+    
+    url = f"https://api.nasa.gov/mars-photos/api/v1/rovers/spirit/photos?sol=1000&api_key={NASA_API_KEY}"
+    
+    try:
+        res = requests.get(url, timeout=15) 
+        res.raise_for_status()
+        data = res.json()
+        
+        photos = data.get('photos', [])[:12]
+        
+        if not photos:
+            return "NASA returned 200 OK but the photo list is empty. Try Sol 500."
+
+        return render_template('mars.html', photos=photos)
+
+    except Exception as e:
+        return f"Network Error: {e}. If you see Name.com, your DNS is blocking NASA."
+
 
 @app.route('/iss')
 def iss():
     return render_template('iss.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
